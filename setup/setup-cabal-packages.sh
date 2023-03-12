7fc92d2ef7e6ecc08e85a803e8782d00cd40bd53
SUZUKI Kei <todokimasen@gmail.com>
2023-03-12 15:09:14 +0900

    cabal update を実行するディレクトリを固定した.

--- setup/setup-cabal-packages.sh
+++ setup/setup-cabal-packages.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
+declare -r ROOT_DIR="$(cd "$(dirname "$0")"/.. && pwd)"
 declare -r SETUP_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 if $(which cabal > /dev/null); then
+    cd "${ROOT_DIR}"
     cabal update
     cabal install --lib HUnit
     cabal clean
