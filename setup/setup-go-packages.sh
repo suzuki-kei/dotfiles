aceb2e9064228307b5ce6b72f259148c82c94a8e
SUZUKI Kei <todokimasen@gmail.com>
2023-11-21 15:16:20 +0900

    セットアップスクリプトを 1 ファイルにまとめた.

--- setup/setup-go-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-if $(which go > /dev/null); then
-    go install golang.org/x/tools/cmd/godoc@latest
-fi
-
--- setup/setup.sh
+++ setup/setup.sh
@@ -5,8 +5,62 @@ set -eu -o pipefail
 declare -r ROOT_DIR="$(cd "$(dirname "$0")"/.. && pwd)"
 declare -r SETUP_DIR="${ROOT_DIR}/setup"
 
-bash "${SETUP_DIR}/setup-dotfiles.sh"
-bash "${SETUP_DIR}/setup-brew-packages.sh"
-bash "${SETUP_DIR}/setup-python-packages.sh"
-bash "${SETUP_DIR}/setup-go-packages.sh"
+function main
+{
+    setup_dotfiles
+    setup_brew_packages
+    setup_python_packages
+    setup_go_packages
+}
+
+function setup_dotfiles
+{
+    declare -r dotfiles_dir="${ROOT_DIR}/dotfiles"
+    declare -r bashrc_path="${dotfiles_dir}/.bashrc"
+
+    if [[ ! -f ~/.bashrc ]]; then
+        touch ~/.bashrc
+    fi
+    if ! grep -E "^source '${bashrc_path}'$" ~/.bashrc > /dev/null; then
+        echo "source '${bashrc_path}'" >> ~/.bashrc
+    fi
+
+    declare -r FILE_NAMES=(
+        .bash_profile
+        .gitconfig
+        .screenrc
+        .vimrc
+        .docker/config.json
+    )
+    for file_name in "${FILE_NAMES[@]}"
+    do
+        if [[ ! -f ~/${file_name} ]]; then
+            cp -p {"${dotfiles_dir}",~}/"${file_name}"
+        fi
+    done
+}
+
+function setup_brew_packages
+{
+    if $(which brew > /dev/null); then
+        brew bundle --cleanup --no-lock --file "${SETUP_DIR}/Brewfile"
+    fi
+}
+
+function setup_python_packages
+{
+    if $(which pip3 > /dev/null); then
+        pip3 install --upgrade pip
+        pip3 install -Ur "${SETUP_DIR}/requirements.txt"
+    fi
+}
+
+function setup_go_packages
+{
+    if $(which go > /dev/null); then
+        go install golang.org/x/tools/cmd/godoc@latest
+    fi
+}
+
+main
 
